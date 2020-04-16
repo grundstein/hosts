2f69e41544cca6edb24600d970bb3f3997a92edb
jascha <jascha@jaeh.at>
2020-04-16 13:30:57 +0200

    add first pieces of config documentation

--- src/index.mjs
+++ src/index.mjs
@@ -8,22 +8,38 @@ export const env = {
 }
 
 // Service config:
+
+// this is the central load balancer and proxy service.
 export const gps = { port: 4343, name: 'grundstein positioning service' }
+
+// this service redirects from http to https and from 8080 to 4343.
 export const grs = { port: 8080, name: 'grundstein redirection service' }
 
+// serves static files.
 export const gms = { port: 2323, name: 'grundstein magic page server' }
+
+// serves prebuilt api bundles.
 export const gas = { port: 2324, name: 'grundstein api server' }
 
-// export const gbs = { host: '127.0.0.1', port: 23523, name: 'grundstein build service' }
-// export const ghs = { host: '127.0.0.1', port: 23524, name: 'grundstein health checker' }
-// export const gul = { host: '127.0.0.1', port: 23525, name: 'grundstein universal logger' }
+// watches, builds and rebuilds pages when they get updated in git
+// export const gbs = { port: 23523, name: 'grundstein build service' }
 
-// hosts config, if you have multiple pods,
-// duplicate the entry and add additional ips to this object.
+// checks the various pages and apis for healthiness. sends warning notifications on error.
+// export const ghs = { port: 23524, name: 'grundstein health checker' }
 
-export const hosts = {
-  '127.0.0.1': {
-    hostname: 'localhost',
+// logging service all other services use to log to.
+// export const gul = { port: 23525, name: 'grundstein universal logger' }
+
+export const hosts = [
+  {
+    // which ip addresses this server is reachable at. used by ssh to connect to the server.
+    // this option will disappear once pods get provisioned through the api.
+    ips: ['127.0.0.1'],
+
+    // the hostnames this pod will serve.
+    hostnames: ['localhost', 'localhorst'],
+
+    // which services should be installed and started on this pod?
     services: {
       gps,
       grs,
@@ -34,8 +50,11 @@ export const hosts = {
       // gbs,
     },
   },
-}
+]
 
+// which repositories should gbs use to build from.
+// these repositories will be watched and rebuilt when pushed to.
+// in the future, the staging branch will also be served.
 export const repos = {
   'dev.grundstein.it': {
     host: 'github.com',
