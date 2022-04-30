ce68193644ddb35e148b000b07657495d7b906ff
jaeh <jascha@jaeh.at>
2022-04-30 16:31:18 +0200

    test/spec: update module exports

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,10 +1,9 @@
 import { is } from '@magic/test'
 
-import config, { gas, gms, gps, env, hosts, repos } from '../src/index.mjs'
+import config, { env, hosts } from '../src/index.mjs'
 
 export default [
   { fn: is.object(config), info: 'config is an object' },
-  { fn: is.object(config.repos), info: 'config.repos is an object' },
   { fn: is.object(config.env), info: 'config.env is an object' },
   { fn: is.object(config.hosts), info: 'config.hosts is an object' },
 
@@ -13,7 +12,4 @@ export default [
 
   { fn: is.deep.equal(config.hosts, hosts), info: 'config.hosts and hosts are equal' },
   { fn: is.empty(hosts), expect: false, info: 'hosts is not empty' },
-
-  { fn: is.deep.equal(config.repos, repos), info: 'config.repos and repos are equal' },
-  { fn: is.empty(repos), expect: false, info: 'repos is not empty' },
 ]
